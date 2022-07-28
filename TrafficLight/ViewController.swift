5b4813c8768bbf6607bf55b510589a5f4ed53c18
Bogdan Anishchenkov <bogdaggi@gmail.com>
2022-07-28 23:26:09 +0300

    Created views, button and set up the outlets

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var lightChangeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func changeLight() {
+    }
+    
+    private func setupLightViews() {
+        
+    }
 
 }
 
